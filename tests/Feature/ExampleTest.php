657cc13a1bfdcc3d49a475c5f6bd0312a61bf46b
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-02-23 14:44:23 +0100

    FINALLY I HAVE WIN

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\News;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,10 +13,18 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+    public function test_post_an_database_news()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+        $response = $this->post('api/v1/news', [
+            'title_fr_new' => 'ceci est un test',
+            'describe_fr_new' => 'blablalbi blablabla',
+            'title_ang_new' => 'do you speak english',
+            'describe_ang_new' => 'yes yes',
+            'icon_new' => "http://image.fr",
+            'position_icon_new' => 3,
+            'fk_Users_News' => 1 ,
+        ]);
+        $response->assertCreated();
+        $this->assertCount(15, News::all());
+}
 }
